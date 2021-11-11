77a60436d33697d1929f4381554aeb78cc0b314f
Joni Parviainen <89932616+Woopsoops@users.noreply.github.com>
2021-11-11 11:21:00 +0200

    Add files via upload

---
+++ src/KyselyApplication.java
@@ -0,0 +1,53 @@
+package com.hh.kysely;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hh.kysely.domain.*;
+
+@SpringBootApplication
+public class KyselyApplication {
+	private static final Logger log = LoggerFactory.getLogger(KyselyApplication.class);
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(KyselyApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner kyselyDemo(KyselyRepo kRepo, KysymysRepo kysymRepo, VastausRepo vRepo)
+	{
+		return (args) -> {
+			log.info("save a couple of question");
+			
+			Kysely kysely1 = new Kysely ("Värit");
+			kRepo.save(kysely1);
+			
+			Kysymys kysymys1 = new Kysymys ("Minkä värinen on musta?", kysely1);
+			kysymRepo.save(kysymys1);
+			
+			Kysymys kysymys2 = new Kysymys ("Minkä värinen on sininen?", kysely1);
+			kysymRepo.save(kysymys2);
+			
+			Vastaus vastaus1 = new Vastaus ("musta", kysymys1);
+			vRepo.save(vastaus1);
+					
+			log.info("fetch all questions");
+			for (Kysymys kysymys : kysymRepo.findAll())
+			{
+				log.info(kysymys.toString());
+			}
+			
+			for (Vastaus vastaus : vRepo.findAll())
+			{
+				log.info(vastaus.toString());
+			}
+			
+		};
+	}
+}
---
+++ src/Kysymys.java
@@ -0,0 +1,74 @@
+package com.hh.kysely.domain;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Kysymys {
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	 private Long kysymysid;
+	 private String kysymys;
+	    
+	 
+	 	@ManyToOne
+	 	@JsonIgnoreProperties ("kysymykset")
+	    @JoinColumn (name= "kyselyid")
+	    private Kysely kysely;
+		
+	    @OneToMany(cascade = CascadeType.ALL, mappedBy = "kysymys")
+	    @JsonIgnoreProperties ("kysymys")
+	    private List<Vastaus> vastaukset;
+	    
+	    public Kysymys () {}
+
+		public Kysymys(String kysymys, Kysely kysely) {
+			super();
+			this.kysymys = kysymys;
+			this.kysely = kysely;
+		}
+
+		public Long getKysymysid() {
+			return kysymysid;
+		}
+
+		public void setKysymysid(Long kysymysid) {
+			this.kysymysid = kysymysid;
+		}
+
+		public String getKysymys() {
+			return kysymys;
+		}
+
+		public void setKysymys(String kysymys) {
+			this.kysymys = kysymys;
+		}
+
+		public Kysely getKysely() {
+			return kysely;
+		}
+
+		public void setKysely(Kysely kysely) {
+			this.kysely = kysely;
+		}
+
+		
+		public List<Vastaus> getVastaukset() {
+			return vastaukset;
+		}
+
+		public void setVastaukset(List<Vastaus> vastaukset) {
+			this.vastaukset = vastaukset;
+		}
+
+		@Override
+		public String toString() {
+			if (this.kysely != null)
+				return "Kysymys [kysymysid=" + kysymysid + ", kysymys=" + kysymys + ", kysely=" + this.getKysely() + "]";
+			else 
+				return "Kysymys [kysymysid=" + kysymysid + ", kysymys=" + kysymys + "]";
+		}
+}
