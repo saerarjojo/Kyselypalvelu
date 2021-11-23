cc358c7cb3f4d1bf070ad3d9761218fdb90dc789
Joni Parviainen <89932616+Woopsoops@users.noreply.github.com>
2021-11-23 09:25:54 +0200

    Add files via upload

--- src/main/java/com/hh/kysely/domain/KysymysRepo.java
+++ src/main/java/com/hh/kysely/domain/KysymysRepo.java
@@ -2,13 +2,13 @@ package com.hh.kysely.domain;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface KysymysRepo extends CrudRepository<Kysymys, Long>
-	{
-
-	    List<Kysymys> findByKysymys(String kysymys);
-	    
+@Repository
+public interface KysymysRepo extends JpaRepository<Kysymys, Long>
+	{	
+	List<Kysely> findByKysymys(String kysymys);
 	}
 
 
