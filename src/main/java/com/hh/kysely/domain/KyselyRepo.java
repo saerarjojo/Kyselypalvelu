cc1bb081bedcad5b02d2501b580ed5665532cef6
Joni Parviainen <89932616+Woopsoops@users.noreply.github.com>
2021-11-25 09:54:16 +0200

    Add files via upload

--- src/main/java/com/hh/kysely/domain/KyselyRepo.java
+++ src/main/java/com/hh/kysely/domain/KyselyRepo.java
@@ -2,9 +2,11 @@ package com.hh.kysely.domain;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface KyselyRepo extends CrudRepository<Kysely, Long>
+@Repository
+public interface KyselyRepo extends JpaRepository<Kysely, Long>
 {
 
     List<Kysely> findByTitle(String title);
